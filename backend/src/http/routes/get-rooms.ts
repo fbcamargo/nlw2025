f30986111c0ae5555107a365e7a18df83e40dad2
Felipe <camargo963@gmail.com>
2025-07-10 22:41:38 -0300

    feat: get rooms join

--- backend/src/http/routes/get-rooms.ts
+++ backend/src/http/routes/get-rooms.ts
@@ -1,6 +1,8 @@
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../db/conection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { count, eq } from "drizzle-orm";
+import { questions } from "../../db/schema/questions.ts";
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get("/rooms", async () => {
@@ -8,8 +10,12 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        questionsCount: count(questions.id),
+        createdAt: schema.rooms.cratedAt,
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id, schema.rooms.name)
       .orderBy(schema.rooms.cratedAt);
     return results;
   });
