c17aa7a14b7f196994db4651fee8a221e43952b8
Felipe <camargo963@gmail.com>
2025-07-10 22:06:01 -0300

    feat: create room

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
 import { sql } from "./db/conection.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -23,6 +24,7 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP server running!`);
